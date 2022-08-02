10e9f9c6013b33814411b16336e8c7f95dc454b7
Dev Agrawal <77338386+DevAgrawal1112@users.noreply.github.com>
2022-08-02 22:27:07 +0530

    Update Suduko_game.cpp

--- Suduko_game.cpp
+++ Suduko_game.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
 #define N 9
-void print(int arr[N][N])
-{
-    for (int i = 0; i < N; i++)
-    {
+void print(int arr[N][N]){
+    for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++)
             cout << arr[i][j] << " ";
         cout << endl;
@@ -12,32 +10,25 @@ void print(int arr[N][N])
 }
 bool isSafe(int grid[N][N], int row,int col, int num)
 {
-    for (int x = 0; x <= 8; x++)
-        if (grid[row][x] == num)
-            return false;
-    for (int x = 0; x <= 8; x++)
-        if (grid[x][col] == num)
-            return false;
-    int startRow = row - row % 3,
-            startCol = col - col % 3;
+    for (int x = 0; x <= 8; x++) if (grid[row][x] == num) return false;
+    for (int x = 0; x <= 8; x++) if (grid[x][col] == num) return false;
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
-            if (grid[i + startRow][j +
-                            startCol] == num)
+            if (grid[i + startRow][j +startCol] == num)
                 return false;
     return true;
 }
-bool solveSuduko(int grid[N][N], int row, int col){
+bool solve(int grid[N][N], int row, int col){
     if (row == N - 1 && col == N) return true;
     if (col == N) {
         row++;
         col = 0;
     }
     if (grid[row][col] > 0) return solveSuduko(grid, row, col + 1);
-    for (int num = 1; num <= N; num++)
-    {  
-        if (isSafe(grid, row, col, num))
-        { 
+    for (int num = 1; num <= N; num++){  
+        if (isSafe(grid, row, col, num)){ 
             grid[row][col] = num;
             if (solveSuduko(grid, row, col + 1))
                 return true;
@@ -46,18 +37,14 @@ bool solveSuduko(int grid[N][N], int row, int col){
     }
     return false;
 } 
-int main()
-{
+int main(){
     int grid[N][N];
     for(int i=0;i<N; i++){
         for(int j=0;j<N;j++){
             cin>>grid[i][j];
         }
     }
- 
-    if (solveSuduko(grid, 0, 0))
-        print(grid);
-    else
-        cout << "No answer exist for this suduko" << endl;
+    if (solve(grid, 0, 0)) print(grid);
+    else cout << "No answer exist for this suduko" << endl;
     return 0;
 }
